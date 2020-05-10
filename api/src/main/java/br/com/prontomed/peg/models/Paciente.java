b001e42a79b7d064591cba9ff9ef3143bf2730a7
Emily Lopez <emiily.jessica@hotmail.com>
2020-05-10 19:37:58 -0300

    acrescentando patologias de base

--- api/src/main/java/br/com/prontomed/peg/models/Paciente.java
+++ api/src/main/java/br/com/prontomed/peg/models/Paciente.java
@@ -5,7 +5,13 @@ import java.util.List;
 public class Paciente extends Pessoa {
 
     private List<String> alergias;
-    private List<String> cirurgias;
+    private boolean dm;
+    private boolean has;
+    private boolean ic;
+    private boolean dpoc;
+    private boolean ca;
+    private boolean etilista;
+    private boolean tabagista;
 
     public List<String> getAlergias() {
         return alergias;
@@ -15,12 +21,60 @@ public class Paciente extends Pessoa {
         this.alergias = alergias;
     }
 
-    public List<String> getCirurgias() {
-        return cirurgias;
+    public boolean isDm() {
+        return dm;
     }
 
-    public void setCirurgias(List<String> cirurgias) {
-        this.cirurgias = cirurgias;
+    public void setDm(boolean dm) {
+        this.dm = dm;
+    }
+
+    public boolean isHas() {
+        return has;
+    }
+
+    public void setHas(boolean has) {
+        this.has = has;
+    }
+
+    public boolean isIc() {
+        return ic;
+    }
+
+    public void setIc(boolean ic) {
+        this.ic = ic;
+    }
+
+    public boolean isDpoc() {
+        return dpoc;
+    }
+
+    public void setDpoc(boolean dpoc) {
+        this.dpoc = dpoc;
+    }
+
+    public boolean isCa() {
+        return ca;
+    }
+
+    public void setCa(boolean ca) {
+        this.ca = ca;
+    }
+
+    public boolean isEtilista() {
+        return etilista;
+    }
+
+    public void setEtilista(boolean etilista) {
+        this.etilista = etilista;
+    }
+
+    public boolean isTabagista() {
+        return tabagista;
+    }
+
+    public void setTabagista(boolean tabagista) {
+        this.tabagista = tabagista;
     }
     
     
---
+++ api/src/main/java/br/com/prontomed/peg/models/Recepcionista.java
@@ -0,0 +1,5 @@
+package br.com.prontomed.peg.models;
+
+public class Recepcionista extends Pessoa {
+    
+}
