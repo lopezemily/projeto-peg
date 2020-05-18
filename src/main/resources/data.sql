efbcd8853d11d1c0532eaf95e4be0ace0a590145
Allan Targino <altargin@microsoft.com>
2020-05-18 09:11:36 -0300

    mudando dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO cid VALUES
     ('R51','Cefaléia');
 
 INSERT INTO medico VALUES
-    ("12300012300","981819191","medico1@medico.com","","","1999-01-01","01770-000","Pq paraiso","Cajamar","Rua abc","21","Medico1","F","100","SP",0,1,1,0,1,1,1),
-    ("22222222200","91111-2222","medico2@medico.com","","","1999-02-02","03770-000","Pq paraiso","Cajamar","Rua def","50","Medico2","M","200","SP",0,0,1,1,1,1,1);;
+    ("12300012300","931819191","medico1@medico.com","","","1999-01-01","01770-000","Lapa","São Paulo","Rua abc","21","Medico1","F","100","SP",0,1,1,0,1,1,1),
+    ("22222222200","91111-2222","medico2@medico.com","","","1999-02-02","03750-000","Lapa","São Paulo","Rua def","50","Medico2","M","200","SP",0,0,1,1,1,1,1);;
 
 INSERT INTO especialidade VALUES
     (1,"Clinica Medica"),
@@ -17,4 +17,4 @@ INSERT INTO medico_especialidades VALUES
     ("12300012300",1);
 
 INSERT INTO unidade VALUES
-    ("999", "99999-9999","unidadeclinica@unidade.com",NULL,"4448-5358","08870-000","Cajamar","Centro","Avenida abc","Clinica PEG");
+    ("999", "99999-9999","unidadeclinica@unidade.com",NULL,"1234-5678","08870-000","São Paulo","Centro","Avenida abc","Clinica PEG");
