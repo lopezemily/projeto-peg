a30e7123bd9ee48aadff8e314d3b44ed79b8f482
Emily Lopez <emiily.jessica@hotmail.com>
2020-05-27 14:49:07 -0300

    implementação de 'Iniciar atendimento' no perfil médico

--- src/main/java/br/com/prontomed/peg/controllers/MedicoController.java
+++ src/main/java/br/com/prontomed/peg/controllers/MedicoController.java
@@ -1,9 +1,11 @@
 package br.com.prontomed.peg.controllers;
 
+import br.com.prontomed.peg.models.Prontuario;
 import br.com.prontomed.peg.services.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +27,19 @@ public class MedicoController {
         modelAndView.addObject("proximasConsultas", consultaService.obterConsultasProximasMedico(cpf));
 
         return modelAndView;
+    }
+    
+    @RequestMapping(value = {"/atender/{numAtendimento}"}, method = RequestMethod.GET)
+    public ModelAndView realizarConsulta(@PathVariable long numAtendimento) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("medico/novaConsulta");
+        return modelAndView;
+    }
+    
+    @RequestMapping(value = {"/atender/{numAtendimento}"}, method = RequestMethod.POST)
+    public String salvarConsulta(@PathVariable long numAtendimento, Prontuario prontuario) {
+        consultaService.registrarProntuario(numAtendimento, prontuario);
+        return "redirect:/medico/home";
     }
 
 }
--- src/main/java/br/com/prontomed/peg/repositories/ConsultaRepository.java
+++ src/main/java/br/com/prontomed/peg/repositories/ConsultaRepository.java
@@ -1,13 +1,17 @@
 package br.com.prontomed.peg.repositories;
 
 import br.com.prontomed.peg.models.Consulta;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
     List<Consulta> findByRealizadaAndPacienteCpf(boolean realizada, String pacienteCpf);
-    
+
     List<Consulta> findByRealizadaAndMedicoCpf(boolean realizada, String medicoCpf);
-    }
+    
+    //Long countByMedicoCpfAndInicioAndFimAndRealizadaAndConfirmada(String medicoCpf, LocalDateTime inicio, LocalDateTime fim, boolean realizada, boolean confirmada);
+}
